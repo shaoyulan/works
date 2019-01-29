02871d40cd4ad242cfa2ba81a88444fde51caf79
shaoyulan <45042818+shaoyulan@users.noreply.github.com>
2019-01-29 11:27:56 +0800

    color update

--- docs/todolist_demo1129_No_PHP/js/action.js
+++ docs/todolist_demo1129_No_PHP/js/action.js
@@ -7,12 +7,15 @@ $(document).ready(function() {
 	//enter editor mode
 	$('#todo-list')
 		.on('dblclick','.content',function(e){
+			// 雙擊之後改成editable 並 focus
 			$(this).prop('contenteditable','true').focus();
+			///////////////////////////////////////////////
 		})
 		//update and create
 		.on('blur','.content',function(e){
-			//判斷是否是class=new
+			//判斷是否是class=new //////////////////////////
 			var isNew = $(this).closest('li').is('.new')
+			////////////////////////////////////////////////
 			if (isNew){
 				//create
 				$('li.new .content').blur(function(e){
@@ -21,10 +24,12 @@ $(document).ready(function() {
 
 					// 使用者有輸入資料才新增li
 					if (todo.length>0){
+						// 物件格式存放資料，準備放入template
 						var todo ={
 							is_complete:false,
 							content:todo,
 						};
+						////////////////////////////////////
 						var li = todoTemplate(todo);
 						$(this).closest('li').before(li);
 
@@ -50,7 +55,9 @@ $(document).ready(function() {
 			$(this).closest('li').toggleClass('complete');
 		});
 
+		// 使用Jquery UI 的sortable
 		$('#todo-list').find('ul').sortable({
 			items:'li:not(.new)',
 		});
+		///////////////////////////////////////
 });
